1efd0686e874982461beab6c55e75601bfbdd766
Medha Gupta <medhagupta312002@gmail.com>
2024-01-31 00:27:23 +0530

    temp

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import Avataaar from "./avatar";
+
+
+const App = () => {
+  const [Attributes, setAttributes] = useState({
+    topType: "ShortHairDreads02",
+    accessoriesType: "Prescription02",
+    hairColor: "BrownDark",
+    facialHairType: "Blank",
+    clotheType: "Hoodie",
+    clotheColor: "PastelBlue",
+    eyeType: "Happy",
+    eyebrowType: "Default",
+    mouthType: "Smile",
+    avatarStyle: "Circle",
+    skinColor: "Light",
+  });
+  return (
+    <div>
+    
+      <Avataaar value={Attributes} onChange={setAttributes} />
+     
+    </div>
+  );
+};
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+app.use(express.static(__dirname)); //here is important thing - no static directory, because all static :)
+
+app.get("/*", function(req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
